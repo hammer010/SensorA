a3a1172237506736150e1f8f1d4b061291335594
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-11-01 23:46:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,6 @@ def timer(length):
             running = False
         else:
             print "Only %.1f more seconds!" % (length - (time.time() - start))
+ 
+if __name__ == '__main__':
+    timer(30)
