5bedd6d90dde01ecf31329eb4d23250cb129ba52
hammer010 <32393979+hammer010@users.noreply.github.com>
2017-11-02 00:09:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import os
 def timer(length, dt=1): 
     end = time.time() + length 
     while(time.time() < end): 
-        print "Only %.1f more seconds!" % (end - time.time()) 
-        time.sleep(dt)
+        print "Only %.1f more seconds!" % (end - time.time())
         os.system('clear')
+        time.sleep(dt)
+        
         print "Time's up!"
 if __name__ == '__main__':
     timer(5)
